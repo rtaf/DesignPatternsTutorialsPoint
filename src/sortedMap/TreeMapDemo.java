129111d8107ed878e39cdb2ced0836536bf0a0c4
Radu Tafuni <rtafuni20@yahoo.com>
2017-01-11 18:14:51 +0200

    sorted map

---
+++ src/sortedMap/TreeMapDemo.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sortedMap;
+
+import java.util.*;
+
+/**
+ *
+ * @author rtafuni
+ */
+public class TreeMapDemo {
+
+    public static void main(String args[]) {
+        //create a hash map
+        TreeMap tm = new TreeMap();
+        
+        //put elements to the map
+        tm.put("Zara", new Double(3434.34));
+
+        
+    }
+}
