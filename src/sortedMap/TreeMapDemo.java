06c9a97be4b4dfb596a4a5d1f7eccbc456a294e0
Radu Tafuni <rtafuni20@yahoo.com>
2017-01-12 17:01:39 +0200

    adding the rest of the code

--- src/sortedMap/TreeMapDemo.java
+++ src/sortedMap/TreeMapDemo.java
@@ -19,7 +19,29 @@ public class TreeMapDemo {
         
         //put elements to the map
         tm.put("Zara", new Double(3434.34));
-
+        tm.put("Mahnaz", new Double(123.22));
+        tm.put("Ayan", new Double(1378.00));
+        tm.put("Daisy", new Double(99.22));
+        tm.put("Qadir", new Double(-19.08));
+        
+        //get a set of the entries
+        Set set = tm.entrySet();
+        
+        //get an iterator
+        Iterator i = set.iterator();
+        
+        //display elements
+        while(i.hasNext()) {
+            Map.Entry me = (Map.Entry)i.next();
+            System.out.println(me.getKey() + " : ");
+            System.out.println(me.getValue());
+        }
+        System.out.println();
+        
+        //Deposit 1000 into Zara's account
+        double balance = ((Double)tm.get("Zara")).doubleValue();
+        tm.put("Zara", new Double(balance + 1000));
+        System.out.println("Zara's new balance: " + tm.get("Zara"));
         
     }
 }
